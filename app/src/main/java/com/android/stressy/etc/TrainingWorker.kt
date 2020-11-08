7aaf103d5985f8bd2a831b69ea229906f5b467aa
sejinnnnnn <sejinnnnnn@naver.com>
2020-11-08 15:16:49 +0900

    trainworker

--- app/src/main/java/com/android/stressy/etc/TrainingWorker.kt
+++ app/src/main/java/com/android/stressy/etc/TrainingWorker.kt
@@ -4,15 +4,30 @@ import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.android.stressy.paillier.KeyPairBuilder
 import kotlinx.coroutines.coroutineScope
 
-class TrainingWorker(appContext: Context, workerParams: WorkerParameters)
-    : CoroutineWorker(appContext, workerParams) {
+class TrainingWorker(appContext: Context, workerParams: WorkerParameters): CoroutineWorker(appContext, workerParams) {
 
-        override suspend fun doWork(): Result = coroutineScope {
+    override suspend fun doWork(): Result = coroutineScope {
 
+        generateKey()
 
-            Log.d("trainingWorker","working")
-            Result.success()
-        }
+        Log.d("trainingWorker", "working")
+        Result.success()
     }
+
+    fun generateKey() {
+
+        val keygen = KeyPairBuilder()
+        val keyPair = keygen.generateKeyPair()
+
+        val publicKey = keyPair.getPublicKey()
+        val privateKey = keyPair.getPrivateKey()
+
+        val publicKey_str = publicKey.toString()
+        val privateKey_str = privateKey.toString()
+
+    }
+
+}
