fde2f811aa5d36109e51c497366f9b3ded85f818
sejin <sejinnnnnn@naver.com>
2020-11-16 05:48:32 +0900

    Update ReceiveWeightWorker.kt

--- app/src/main/java/com/android/stressy/etc/ReceiveWeightWorker.kt
+++ app/src/main/java/com/android/stressy/etc/ReceiveWeightWorker.kt
@@ -18,29 +18,41 @@ class ReceiveWeightWorker(appContext: Context, workerParams: WorkerParameters)
     val mPref = "my_pref"
     val prefs = context.getSharedPreferences(mPref,Context.MODE_PRIVATE)
 
-        override suspend fun doWork(): Result = coroutineScope {
-            Log.d("rwrw","received")
+    override suspend fun doWork(): Result = coroutineScope {
+        Log.d("rwrw","received")
 
-//            withVolley()
+        withVolley()
 
-            Result.success()
-        }
+        Result.success()
+    }
 
-    fun withVolley(jsonObject: JSONObject){
-        val prefs = context.getSharedPreferences("my_pref",Context.MODE_PRIVATE)
-        val fcm_token = prefs.getString("pref_fcm_token",null).toString()
-        val jwt_token = prefs.getString("jwt",null).toString()
-        jsonObject.put("fcm_token",fcm_token)
+    fun updateLocalModel(W_0: JSONObject, RW_0: JSONObject, b_0: JSONObject, W_2: JSONObject, b_2: JSONObject) {
+
+        Log.d("rw_0W", W_0.toString())
+        Log.d("rw_0RW", RW_0.toString())
+        Log.d("rw_0b", b_0.toString())
+        Log.d("rw_2W", W_2.toString())
+        Log.d("rw_2b", b_2.toString())
+
+    }
+
+    fun withVolley() {
 
         val url = BaseUrl.url_aggregate + "/receive_weights"
         val queue = Volley.newRequestQueue(context)
-
+        val jsonObject = JSONObject()
         val jsonRequest = object : JsonObjectRequest(
             Method.POST,url,jsonObject,
             Response.Listener<JSONObject> { res ->
                 val jsonObject = res
+                val W_0 = jsonObject.getJSONObject("finalWeights_0W")
+                val RW_0 = jsonObject.getJSONObject("finalWeights_0RW")
+                val b_0 = jsonObject.getJSONObject("finalWeights_0b")
+                val W_2 = jsonObject.getJSONObject("finalWeights_2W")
+                val b_2 = jsonObject.getJSONObject("finalWeights_2b")
 
-                Log.d("rw:res", res.toString())
+                updateLocalModel(W_0, RW_0, b_0, W_2, b_2)
+
             },
             Response.ErrorListener { error ->
                 Log.d("rw.error", error.toString())
